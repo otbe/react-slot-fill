61213a189d736a28b3f624953521645e74755df2
Benjamin Otto <benjamin.otto@style-systems.de>
2017-08-08 18:11:55 +0200

    added build

---
+++ lib/components/Provider.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="react" />
+import * as React from 'react';
+import mitt from 'mitt';
+import { Requireable } from 'prop-types';
+import Manager from '../Manager';
+import Fill from './Fill';
+export default class Provider extends React.Component<{}, {}> {
+    static childContextTypes: {
+        manager: Requireable<any>;
+        bus: Requireable<any>;
+    };
+    private _bus;
+    private _manager;
+    constructor();
+    componentWillUnmount(): void;
+    getChildContext(): {
+        bus: mitt.Emitter;
+        manager: Manager;
+    };
+    render(): React.ReactElement<any>;
+    /**
+     * Returns instances of Fill react components
+     */
+    getFillsByName(name: string): Fill[];
+    /**
+     * Return React elements that were inside Fills
+     */
+    getChildrenByName(name: string): React.ReactChild[];
+}
